308c7ba34b999c9fa41cc1ae036f98ec86af66d2
Charlie <crck@outlook.com>
2015-06-16 15:54:24 +0100

    Adds delete functionality

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -27,6 +27,13 @@ class RestaurantsController < ApplicationController
     redirect_to '/restaurants'
   end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to '/restaurants'
+  end
+
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
