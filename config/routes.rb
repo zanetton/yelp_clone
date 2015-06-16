199dbc1170317b9649a7726ca0529c6bd65a2a41
Joanne Zanetton <joannezanetton@gmail.com>
2015-06-16 17:32:02 +0100

    adds reviews, method missing

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   # get 'restaurants' => 'restaurants#index'
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end
 end
