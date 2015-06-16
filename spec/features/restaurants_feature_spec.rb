8f1c5f8d6d14ed6789d19a7933bc119de97258bd
Joanne Zanetton <joannezanetton@gmail.com>
2015-06-16 12:50:34 +0100

    Adds edit and update functionality

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -43,6 +43,20 @@ feature 'restaurants' do
    expect(page).to have_content 'KFC'
    expect(current_path).to eq "/restaurants/#{kfc.id}"
   end
-
 end
+
+  context 'editing restaurants' do
+
+    before {Restaurant.create name: 'KFC'}
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
 end
