cb5afe6774a8f4e9f7d751527c588e16e7562883
Charlie <crck@outlook.com>
2015-06-17 10:22:07 +0100

    Adds validations for review rating <= 5

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -4,15 +4,21 @@ describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
 
   it 'has no reviews if deleted' do
-    restaurant = Restaurant.create(name: 'KFC')
-    Review.create(thoughts: 'Good', rating: 5, restaurant_id: restaurant.id)
-    expect { restaurant.destroy}.to change {Review.count}.by(-1)
+      restaurant = Restaurant.create(name: 'KFC')
+      Review.create(thoughts: 'Good', rating: 5, restaurant_id: restaurant.id)
+      expect { restaurant.destroy}.to change {Review.count}.by(-1)
   end
 
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
+  end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
   end
 end
 
