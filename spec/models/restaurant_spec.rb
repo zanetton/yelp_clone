9329cea4ba71ff201f55cf81b70dd1bcf0f83658
Charlie <crck@outlook.com>
2015-06-17 09:58:33 +0100

    Adds shoulda test for deleting restaurants

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -4,9 +4,9 @@ describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
 
   it 'has no reviews if deleted' do
-    restaurant = Restaurant.new
-    restaurant.destroy
-    expect(restaurant).not_to include :reviews
+    restaurant = Restaurant.create(name: 'KFC')
+    Review.create(thoughts: 'Good', rating: 5, restaurant_id: restaurant.id)
+    expect { restaurant.destroy}.to change {Review.count}.by(-1)
   end
 end
 
