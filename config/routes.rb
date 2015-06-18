9b6388a3442cf95c68ee4757a5361dbfe418efd1
Joanne Zanetton <joannezanetton@gmail.com>
2015-06-18 17:51:18 +0100

    Adding endorsements

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "restaurants#index"
 
+
+
   # devise_scope :user do
   #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
@@ -63,7 +65,9 @@ Rails.application.routes.draw do
   #   end
   # get 'restaurants' => 'restaurants#index'
   resources :restaurants do
-    resources :reviews
+    resources :reviews do
+      resources :endorsements, shallow: true
+    end
   end
 
 end
---
+++ spec/features/endorsements_feature_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'endorsing reviews' do
+  before do
+    user_sign_up('test@example.com')
+    create_restaurant('kfc')
+    click_link 'Review kfc'
+    leave_review('It was an abomination', '3')
+  end
+
+  scenario 'a user can endorse a review, which updates the review endorsement count' do
+    visit '/restaurants'
+    click_link 'Endorse KFC'
+    expect(page).to have_content('1 endorsement')
+  end
+
+  scenario 'several users can endorse a review, which updates the review endorsement count' do
+    visit '/restaurants'
+    click_link 'Endorse KFC'
+    click_link 'Sign out'
+    user_sign_up('test2@example.com')
+    click_link 'Endorse KFC'
+    click_link 'Sign out'
+    user_sign_up('test3@example.com')
+    click_link 'Endorse KFC'
+    expect(page).to have_content('3 endorsements')
+  end
+end
